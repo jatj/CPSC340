a82e0be5e2a3b10140c9c41cbc2ce2e80452036a
JATJ <a01420804@itesm.mx>
2019-01-27 00:34:26 -0800

    Started assignment 3

--- code/main.py
+++ code/main.py
@@ -1,362 +1,152 @@
+
 # basics
+import argparse
 import os
 import pickle
-import argparse
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 
 # sklearn imports
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import normalize
 
 # our code
+import linear_model
 import utils
 
-from knn import KNN
-
-from naive_bayes import NaiveBayes
-
-from decision_stump import DecisionStumpErrorRate, DecisionStumpEquality, DecisionStumpInfoGain
-from decision_tree import DecisionTree
-from random_tree import RandomTree
-from random_forest import RandomForest
-
-from kmeans import Kmeans
-from sklearn.cluster import DBSCAN
+url_amazon = "https://www.amazon.com/dp/%s"
 
 def load_dataset(filename):
     with open(os.path.join('..','data',filename), 'rb') as f:
         return pickle.load(f)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-q','--question', required=True)
-
     io_args = parser.parse_args()
     question = io_args.question
 
-
     if question == "1":
-        with open(os.path.join('..','data','citiesSmall.pkl'), 'rb') as f:
-            dataset = pickle.load(f)
 
-        X, y = dataset["X"], dataset["y"]
-        X_test, y_test = dataset["Xtest"], dataset["ytest"]        
-        model = DecisionTreeClassifier(max_depth=2, criterion='entropy', random_state=1)
-        model.fit(X, y)
+        filename = "ratings_Patio_Lawn_and_Garden.csv"
+        with open(os.path.join("..", "data", filename), "rb") as f:
+            ratings = pd.read_csv(f,names=("user","item","rating","timestamp"))
 
-        y_pred = model.predict(X)
-        tr_error = np.mean(y_pred != y)
+        print("Number of ratings:", len(ratings))
+        print("The average rating:", np.mean(ratings["rating"]))
 
-        y_pred = model.predict(X_test)
-        te_error = np.mean(y_pred != y_test)
-        print("Training error: %.3f" % tr_error)
-        print("Testing error: %.3f" % te_error)
+        n = len(set(ratings["user"]))
+        d = len(set(ratings["item"]))
+        print("Number of users:", n)
+        print("Number of items:", d)
+        print("Fraction nonzero:", len(ratings)/(n*d))
+
+        X, user_mapper, item_mapper, user_inverse_mapper, item_inverse_mapper, user_ind, item_ind = utils.create_user_item_matrix(ratings)
+        print(type(X))
+        print("Dimensions of X:", X.shape)
 
     elif question == "1.1":
-        with open(os.path.join('..','data','citiesSmall.pkl'), 'rb') as f:
-            dataset = pickle.load(f)
-
-        X, y = dataset["X"], dataset["y"]
-        X_test, y_test = dataset["Xtest"], dataset["ytest"]
-
-        testErrors = np.zeros(15)
-        trainingErrors = np.zeros(15)
-        depths = range(1,16)
-        for d in depths:
-            model = DecisionTreeClassifier(max_depth=d, criterion='entropy', random_state=1)
-            model.fit(X, y)
-
-            y_pred = model.predict(X)
-            tr_error = np.mean(y_pred != y)
-
-            y_pred = model.predict(X_test)
-            te_error = np.mean(y_pred != y_test)
-
-            testErrors[d-1] = te_error
-            trainingErrors[d-1] = tr_error
-
-        plt.plot(depths, trainingErrors, label="Training")
-        plt.plot(depths, testErrors, label="Testing", linestyle=":", linewidth=3)
-        plt.xlabel("Depth of tree")
-        plt.ylabel("Error")
-        plt.legend()
-        fname = os.path.join("..", "figs", "q1_1_errors.pdf")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-
-
-
-    elif question == '1.2':
-        with open(os.path.join('..','data','citiesSmall.pkl'), 'rb') as f:
-            dataset = pickle.load(f)
-
-        X, y = dataset["X"], dataset["y"]
-        n, d = X.shape
-        half_n = int(n/2)
-        Xs = [X[0:half_n],X[half_n:]]
-        Ys = [y[0:half_n],y[half_n:]]
-
-        for i in range(2):
-            Xtrain = Xs[i]
-            ytrain = Ys[i]
-
-            Xvalidation = Xs[(i+1)%2]
-            yvalidation = Ys[(i+1)%2]
-
-            validationErrors = np.zeros(15)
-            trainingErrors = np.zeros(15)
-            depths = range(1,16)
-            for d in depths:
-                model = DecisionTreeClassifier(max_depth=d, criterion='entropy', random_state=1)
-                model.fit(Xtrain, ytrain)
-
-                y_pred = model.predict(Xtrain)
-                tr_error = np.mean(y_pred != ytrain)
-
-                y_pred = model.predict(Xvalidation)
-                va_error = np.mean(y_pred != yvalidation)
-
-                validationErrors[d-1] = va_error
-                trainingErrors[d-1] = tr_error
-
-            plt.plot(depths, trainingErrors, label="Training")
-            plt.plot(depths, validationErrors, label="Validation", linestyle=":", linewidth=3)
-
-            diffError = validationErrors - trainingErrors
-            aveError = (validationErrors + trainingErrors)/2
-            stats_rows = ["gap","validation", "train", "average"]
-            stats_cols = range(1,16)
-            stats = pd.DataFrame(index=stats_rows, columns=stats_cols, data = [
-                diffError,
-                validationErrors,
-                trainingErrors,
-                aveError
-            ])
-            print(stats)
-
-            plt.xlabel("Depth of tree")
-            plt.ylabel("Error")
-            plt.title("Error over depth")
-            plt.legend()
-            fname = os.path.join("..", "figs", "q1_2_training_validation_{}.pdf".format(i+1))
-            plt.savefig(fname)
-            plt.clf()
-            print("\nFigure saved as '%s'" % fname)
-
-
-
-    elif question == '2.2':
-        dataset = load_dataset("newsgroups.pkl")
-
-        X = dataset["X"]
-        y = dataset["y"]
-        X_valid = dataset["Xvalidate"]
-        y_valid = dataset["yvalidate"]
-        groupnames = dataset["groupnames"]
-        wordlist = dataset["wordlist"]
-
-        # ############################ 2.2.1
-        print("\n############################ 2.2.1\n")
-        print("The word of column 51: {}".format(wordlist[50]))
-
-        # ############################ 2.2.2
-        print("\n############################ 2.2.2\n")
-        print("Words present in training example 501:")
-        print(wordlist[X[500] > 0])
-
-        # ############################ 2.2.3
-        print("\n############################ 2.2.3\n")
-        print("Newsgroup name of training example 501:")
-        print(groupnames[y[500]])
-
-    elif question == '2.3':
-        dataset = load_dataset("newsgroups.pkl")
-
-        X = dataset["X"]
-        y = dataset["y"]
-        X_valid = dataset["Xvalidate"]
-        y_valid = dataset["yvalidate"]
-
-        print("d = %d" % X.shape[1])
-        print("n = %d" % X.shape[0])
-        print("t = %d" % X_valid.shape[0])
-        print("Num classes = %d" % len(np.unique(y)))
-
-        model = NaiveBayes(num_classes=4)
-        model.fit(X, y)
-        y_pred = model.predict(X_valid)
-        v_error = np.mean(y_pred != y_valid)
-        print("Naive Bayes (ours) validation error: %.3f" % v_error)
-
-        # Scikit learn BernoulliNB
-        sciKit_naive_bayes = BernoulliNB()
-        sciKit_naive_bayes.fit(X,y)
-        y_pred = sciKit_naive_bayes.predict(X_valid)
-        v_error = np.mean(y_pred != y_valid)
-        print("Naive Bayes (scikit) validation error: %.3f" % v_error)
-    
-
-    elif question == '3':
-        with open(os.path.join('..','data','citiesSmall.pkl'), 'rb') as f:
-            dataset = pickle.load(f)
-
-        X = dataset['X']
-        y = dataset['y']
-        Xtest = dataset['Xtest']
-        ytest = dataset['ytest']
-        knnModels = []
-        knnScikitModels = []
-
-        for k in [1,3,10]:
-            print("\n------ K={} -----\n".format(k))
-            
-            modelKNN = KNN(k)
-            modelKNN.fit(X,y)
-            y_pred = modelKNN.predict(X)
-            training_error = np.mean(y_pred != y)
-            y_pred = modelKNN.predict(Xtest)
-            test_error = np.mean(y_pred != ytest)
-            print("KNN (k={:d}) training_error: {:.3f}".format(k, training_error))
-            print("KNN (k={:d}) test_error: {:.3f}".format(k,test_error))
-
-            modelKNN_Scikit = KNeighborsClassifier(n_neighbors=k)
-            modelKNN_Scikit.fit(X,y)
-            y_pred = modelKNN_Scikit.predict(X)
-            training_error = np.mean(y_pred != y)
-            y_pred = modelKNN_Scikit.predict(Xtest)
-            test_error = np.mean(y_pred != ytest)
-            print("KNN Scikit (k={:d}) training_error: {:.3f}".format(k, training_error))
-            print("KNN Scikit (k={:d}) test_error: {:.3f}".format(k, test_error))
-
-            knnModels.append(modelKNN)
-            knnScikitModels.append(modelKNN_Scikit)
-
-        utils.plotClassifier(knnModels[0],X,y)
-        fname = os.path.join("..", "figs", "q3_3_myKNN.pdf")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-        plt.clf()
-
-        utils.plotClassifier(knnScikitModels[0],X,y)
-        fname = os.path.join("..", "figs", "q3_3_scikitKNN.pdf")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-
-
-    elif question == '4':
-        dataset = load_dataset('vowel.pkl')
-        X = dataset['X']
-        y = dataset['y']
-        X_test = dataset['Xtest']
-        y_test = dataset['ytest']
-        print("\nn = %d, d = %d\n" % X.shape)
-
-        def evaluate_model(model):
-            model.fit(X,y)
-
-            y_pred = model.predict(X)
-            tr_error = np.mean(y_pred != y)
-
-            y_pred = model.predict(X_test)
-            te_error = np.mean(y_pred != y_test)
-            print("    Training error: %.3f" % tr_error)
-            print("    Testing error: %.3f" % te_error)
-
-        print("Decision tree info gain")
-        evaluate_model(DecisionTree(max_depth=np.inf, stump_class=DecisionStumpInfoGain))
-        print("Random tree")
-        evaluate_model(RandomTree(np.inf))
-        print("Random forest")
-        evaluate_model(RandomForest(np.inf,50))
-        print("Random forest scikit")
-        evaluate_model(RandomForestClassifier(n_estimators=50))
-
-
-
-    elif question == '5':
-        X = load_dataset('clusterData.pkl')['X']
-
-        model = Kmeans(k=4)
-        model.fit(X)
-        y = model.predict(X)
-        plt.scatter(X[:,0], X[:,1], c=y, cmap="jet")
-
-        fname = os.path.join("..", "figs", "kmeans_basic.png")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-
-    elif question == '5.1':
-        X = load_dataset('clusterData.pkl')['X']
-        lowestError = np.inf
-        lowestKMeans = None
-        for i in range(50):
-            model = Kmeans(k=4)
-            model.fit(X)
-            error = model.error(X)
-            if error < lowestError:
-                lowestError = error
-                lowestKMeans = model
-    
-        print("KMeans lowest error: {}".format(lowestError))
-        y = lowestKMeans.predict(X)
-        plt.scatter(X[:,0], X[:,1], c=y, cmap="jet")
-
-        fname = os.path.join("..", "figs", "q_5_1_kmeans.pdf")
-        plt.title("Cluster Plot")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-
-
-    elif question == '5.2':
-        X = load_dataset('clusterData.pkl')['X']
-        lowestErrors = []
-        ks = range(1,11)
-        for k in ks:
-            lowestError = np.inf
-            for i in range(50):
-                model = Kmeans(k=k)
-                model.fit(X)
-                error = model.error(X)
-                if error < lowestError:
-                    lowestError = error
-                    lowestKMeans = model
-            lowestErrors.append(lowestError)
-
-
-        plt.plot(ks, lowestErrors, label="Minimum errors")
-        plt.xlabel("K")
-        plt.ylabel("Error")
-        plt.legend()
-        plt.title("Kmeans errors over k")
-        fname = os.path.join("..", "figs", "q5_2_Kmeans_errors.pdf")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-
-
-    elif question == '5.3':
-        X = load_dataset('clusterData2.pkl')['X']
-
-        model = DBSCAN(eps=1, min_samples=3)
-        y = model.fit_predict(X)
-
-        print("Labels (-1 is unassigned):", np.unique(model.labels_))
+        filename = "ratings_Patio_Lawn_and_Garden.csv"
+        with open(os.path.join("..", "data", filename), "rb") as f:
+            ratings = pd.read_csv(f,names=("user","item","rating","timestamp"))
+        X, user_mapper, item_mapper, user_inverse_mapper, item_inverse_mapper, user_ind, item_ind = utils.create_user_item_matrix(ratings)
+        X_binary = X != 0
         
-        plt.scatter(X[:,0], X[:,1], c=y, cmap="jet", s=5)
-        fname = os.path.join("..", "figs", "density.png")
-        plt.savefig(fname)
-        print("\nFigure saved as '%s'" % fname)
-        
-        plt.xlim(-25,25)
-        plt.ylim(-15,30)
-        fname = os.path.join("..", "figs", "density2.png")
-        plt.savefig(fname)
-        print("Figure saved as '%s'" % fname)
-        
+        # YOUR CODE HERE FOR Q1.1.1
+
+        # YOUR CODE HERE FOR Q1.1.2
+
+        # YOUR CODE HERE FOR Q1.1.3
+
+    elif question == "1.2":
+        filename = "ratings_Patio_Lawn_and_Garden.csv"
+        with open(os.path.join("..", "data", filename), "rb") as f:
+            ratings = pd.read_csv(f,names=("user","item","rating","timestamp"))
+        X, user_mapper, item_mapper, user_inverse_mapper, item_inverse_mapper, user_ind, item_ind = utils.create_user_item_matrix(ratings)
+        X_binary = X != 0
+
+        grill_brush = "B00CFM0P7Y"
+        grill_brush_ind = item_mapper[grill_brush]
+        grill_brush_vec = X[:,grill_brush_ind]
+
+        print(url_amazon % grill_brush)
+
+        # YOUR CODE HERE FOR Q1.2
+
+
+
+        # YOUR CODE HERE FOR Q1.3
+
+
+    elif question == "3":
+        data = load_dataset("outliersData.pkl")
+        X = data['X']
+        y = data['y']
+
+        # Fit least-squares estimator
+        model = linear_model.LeastSquares()
+        model.fit(X,y)
+        print(model.w)
+
+        utils.test_and_plot(model,X,y,title="Least Squares",filename="least_squares_outliers.pdf")
+
+    elif question == "3.1":
+        data = load_dataset("outliersData.pkl")
+        X = data['X']
+        y = data['y']
+
+        # YOUR CODE HERE
+
+    elif question == "3.3":
+        # loads the data in the form of dictionary
+        data = load_dataset("outliersData.pkl")
+        X = data['X']
+        y = data['y']
+
+        # Fit least-squares estimator
+        model = linear_model.LinearModelGradient()
+        model.fit(X,y)
+        print(model.w)
+
+        utils.test_and_plot(model,X,y,title="Robust (L1) Linear Regression",filename="least_squares_robust.pdf")
+
+    elif question == "4":
+        data = load_dataset("basisData.pkl")
+        X = data['X']
+        y = data['y']
+        Xtest = data['Xtest']
+        ytest = data['ytest']
+
+        # Fit least-squares model
+        model = linear_model.LeastSquares()
+        model.fit(X,y)
+
+        utils.test_and_plot(model,X,y,Xtest,ytest,title="Least Squares, no bias",filename="least_squares_no_bias.pdf")
+
+    elif question == "4.1":
+        data = load_dataset("basisData.pkl")
+        X = data['X']
+        y = data['y']
+        Xtest = data['Xtest']
+        ytest = data['ytest']
+
+        # YOUR CODE HERE
+
+    elif question == "4.2":
+        data = load_dataset("basisData.pkl")
+        X = data['X']
+        y = data['y']
+        Xtest = data['Xtest']
+        ytest = data['ytest']
+
+        for p in range(11):
+            print("p=%d" % p)
+
+            # YOUR CODE HERE
+
     else:
         print("Unknown question: %s" % question)
+
--- code/utils.py
+++ code/utils.py
@@ -1,99 +1,92 @@
-import os.path
+import pickle
+import os
+import sys
 import numpy as np
+from scipy.optimize import approx_fprime
 import matplotlib.pyplot as plt
-from scipy import stats
+import pandas as pd
+from scipy.sparse import csr_matrix as sparse_matrix
+
+def create_user_item_matrix(ratings,user_key="user",item_key="item"):
+
+    n = len(set(ratings[user_key]))
+    d = len(set(ratings[item_key]))
+
+    user_mapper = dict(zip(np.unique(ratings[user_key]), list(range(n))))
+    item_mapper = dict(zip(np.unique(ratings[item_key]), list(range(d))))
+
+    user_inverse_mapper = dict(zip(list(range(n)), np.unique(ratings[user_key])))
+    item_inverse_mapper = dict(zip(list(range(d)), np.unique(ratings[item_key])))
+
+    user_ind = [user_mapper[i] for i in ratings[user_key]]
+    item_ind = [item_mapper[i] for i in ratings[item_key]]
+
+    X = sparse_matrix((ratings["rating"], (user_ind, item_ind)), shape=(n,d))
+    
+    return X, user_mapper, item_mapper, user_inverse_mapper, item_inverse_mapper, user_ind, item_ind        
+
+def standardize_cols(X, mu=None, sigma=None):
+    # Standardize each column with mean 0 and variance 1
+    n_rows, n_cols = X.shape
+
+    if mu is None:
+        mu = np.mean(X, axis=0)
+
+    if sigma is None:
+        sigma = np.std(X, axis=0)
+        sigma[sigma < 1e-8] = 1.
+
+    return (X - mu) / sigma, mu, sigma
 
 
-def plotClassifier(model, X, y):
-    """plots the decision boundary of the model and the scatterpoints
-       of the target values 'y'.
+def check_gradient(model, X, y):
+    # This checks that the gradient implementation is correct
+    w = np.random.rand(model.w.size)
+    f, g = model.funObj(w, X, y)
 
-    Assumptions
-    -----------
-    y : it should contain two classes: '1' and '2'
+    # Check the gradient
+    estimated_gradient = approx_fprime(w,
+                                       lambda w: model.funObj(w,X,y)[0],
+                                       epsilon=1e-6)
 
-    Parameters
-    ----------
-    model : the trained model which has the predict function
+    implemented_gradient = model.funObj(w, X, y)[1]
 
-    X : the N by D feature array
-
-    y : the N element vector corresponding to the target values
-
-    """
-    x1 = X[:, 0]
-    x2 = X[:, 1]
-
-    x1_min, x1_max = int(x1.min()) - 1, int(x1.max()) + 1
-    x2_min, x2_max = int(x2.min()) - 1, int(x2.max()) + 1
-
-    x1_line =  np.linspace(x1_min, x1_max, 200)
-    x2_line =  np.linspace(x2_min, x2_max, 200)
-
-    x1_mesh, x2_mesh = np.meshgrid(x1_line, x2_line)
-
-    mesh_data = np.c_[x1_mesh.ravel(), x2_mesh.ravel()]
-
-    y_pred = model.predict(mesh_data)
-    y_pred = np.reshape(y_pred, x1_mesh.shape)
-
-    plt.figure()
-    plt.xlim([x1_mesh.min(), x1_mesh.max()])
-    plt.ylim([x2_mesh.min(), x2_mesh.max()])
-
-    plt.contourf(x1_mesh, x2_mesh, -y_pred.astype(int), # unsigned int causes problems with negative sign... o_O
-                cmap=plt.cm.RdBu, alpha=0.6)
-
-    plt.scatter(x1[y==0], x2[y==0], color="b", label="class 0")
-    plt.scatter(x1[y==1], x2[y==1], color="r", label="class 1")
-    plt.legend()
-
-
-def mode(y):
-    """Computes the element with the maximum count
-
-    Parameters
-    ----------
-    y : an input numpy array
-
-    Returns
-    -------
-    y_mode :
-        Returns the element with the maximum count
-    """
-    if len(y)==0:
-        return -1
+    if np.max(np.abs(estimated_gradient - implemented_gradient) > 1e-4):
+        raise Exception('User and numerical derivatives differ:\n%s\n%s' %
+             (estimated_gradient[:5], implemented_gradient[:5]))
     else:
-        return stats.mode(y.flatten())[0][0]
+        print('User and numerical derivatives agree.')
+
+def classification_error(y, yhat):
+    return np.mean(y!=yhat)
 
 
-def euclidean_dist_squared(X, Xtest):
-    """Computes the Euclidean distance between rows of 'X' and rows of 'Xtest'
+def test_and_plot(model,X,y,Xtest=None,ytest=None,title=None,filename=None):
 
-    Parameters
-    ----------
-    X : an N by D numpy array
-    Xtest: an T by D numpy array
+    # Compute training error
+    yhat = model.predict(X)
+    trainError = np.mean((yhat - y)**2)
+    print("Training error = %.1f" % trainError)
 
-    Returns: an array of size N by T containing the pairwise squared Euclidean distances.
+    # Compute test error
+    if Xtest is not None and ytest is not None:
+        yhat = model.predict(Xtest)
+        testError = np.mean((yhat - ytest)**2)
+        print("Test error     = %.1f" % testError)
 
-    Python/Numpy (and other numerical languages like Matlab and R)
-    can be slow at executing operations in `for' loops, but allows extremely-fast
-    hardware-dependent vector and matrix operations. By taking advantage of SIMD registers and
-    multiple cores (and faster matrix-multiplication algorithms), vector and matrix operations in
-    Numpy will often be several times faster than if you implemented them yourself in a fast
-    language like C. The following code will form a matrix containing the squared Euclidean
-    distances between all training and test points. If the output is stored in D, then
-    element D[i,j] gives the squared Euclidean distance between training point
-    i and testing point j. It exploits the identity (a-b)^2 = a^2 + b^2 - 2ab.
-    The right-hand-side of the above is more amenable to vector/matrix operations.
-    """
+    # Plot model
+    plt.figure()
+    plt.plot(X,y,'b.')
 
-    return np.sum(X**2, axis=1)[:,None] + np.sum(Xtest**2, axis=1)[None] - 2 * np.dot(X,Xtest.T)
+    # Choose points to evaluate the function
+    Xgrid = np.linspace(np.min(X),np.max(X),1000)[:,None]
+    ygrid = model.predict(Xgrid)
+    plt.plot(Xgrid, ygrid, 'g')
 
-    # without broadcasting:
-    # n,d = X.shape
-    # t,d = Xtest.shape
-    # D = X**2@np.ones((d,t)) + np.ones((n,d))@(Xtest.T)**2 - 2*X@Xtest.T
+    if title is not None:
+        plt.title(title)
 
-
+    if filename is not None:
+        filename = os.path.join("..", "figs", filename)
+        print("Saving", filename)
+        plt.savefig(filename)
