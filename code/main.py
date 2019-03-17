b05a55318fe5f3b37ce9e488d0ce62aa592ca49b
Abraham Torres <jabraham@gmail.com>
2019-03-17 10:18:06 -0700

    Assignment 5

--- code/main.py
+++ code/main.py
@@ -1,155 +1,112 @@
+import os
+import pickle
 import argparse
+import matplotlib.pyplot as plt
 import numpy as np
-import sys
+from numpy.linalg import norm
+from sklearn.model_selection import train_test_split
 
 import utils
-import linear_model
+import logReg
+from logReg import logRegL2, kernelLogRegL2
+from pca import PCA, AlternativePCA, RobustPCA
 
-from sklearn.linear_model import LogisticRegression
+def load_dataset(filename):
+    with open(os.path.join('..','data',filename), 'rb') as f:
+        return pickle.load(f)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-q','--question', required = True)
+    parser.add_argument('-q','--question', required=True)
+
     io_args = parser.parse_args()
     question = io_args.question
+    
+    if question == "1":
+        dataset = load_dataset('nonLinearData.pkl')
+        X = dataset['X']
+        y = dataset['y']
 
+        Xtrain, Xtest, ytrain, ytest = train_test_split(X,y,random_state=0)
 
-    if question == "2":
-        data = utils.load_dataset("logisticData")
-        XBin, yBin = data['X'], data['y']
-        XBinValid, yBinValid = data['Xvalid'], data['yvalid']
+        # standard logistic regression
+        lr = logRegL2(lammy=1)
+        lr.fit(Xtrain, ytrain)
 
-        model = linear_model.logReg(maxEvals=400)
-        model.fit(XBin,yBin)
+        print("Training error %.3f" % np.mean(lr.predict(Xtrain) != ytrain))
+        print("Validation error %.3f" % np.mean(lr.predict(Xtest) != ytest))
 
-        print("\nlogReg Training error %.3f" % utils.classification_error(model.predict(XBin), yBin))
-        print("logReg Validation error %.3f" % utils.classification_error(model.predict(XBinValid), yBinValid))
-        print("# nonZeros: %d" % (model.w != 0).sum())
-
-    elif question == "2.1":
-        data = utils.load_dataset("logisticData")
-        XBin, yBin = data['X'], data['y']
-        XBinValid, yBinValid = data['Xvalid'], data['yvalid']
-
-        model = linear_model.logRegL2(lammy=1.0, maxEvals=400, verbose=True)
-        model.fit(XBin,yBin)
-
-        print("\nlogRegL2 Training error %.3f" % utils.classification_error(model.predict(XBin), yBin))
-        print("logRegL2 Validation error %.3f" % utils.classification_error(model.predict(XBinValid), yBinValid))
-        print("# nonZeros: %d" % (model.w != 0).sum())
-
-    elif question == "2.2":
-        data = utils.load_dataset("logisticData")
-        XBin, yBin = data['X'], data['y']
-        XBinValid, yBinValid = data['Xvalid'], data['yvalid']
-
-        model = linear_model.logRegL1(L1_lambda=1.0, maxEvals=400, verbose=True)
-        model.fit(XBin,yBin)
-
-        print("\nlogRegL1 Training error %.3f" % utils.classification_error(model.predict(XBin),yBin))
-        print("logRegL1 Validation error %.3f" % utils.classification_error(model.predict(XBinValid), yBinValid))
-        print("# nonZeros: %d" % (model.w != 0).sum())
-
-    elif question == "2.3":
-        data = utils.load_dataset("logisticData")
-        XBin, yBin = data['X'], data['y']
-        XBinValid, yBinValid = data['Xvalid'], data['yvalid']
-
-        model = linear_model.logRegL0(L0_lambda=1.0, maxEvals=400)
-        model.fit(XBin,yBin)
-
-        print("\nTraining error %.3f" % utils.classification_error(model.predict(XBin),yBin))
-        print("Validation error %.3f" % utils.classification_error(model.predict(XBinValid), yBinValid))
-        print("# nonZeros: %d" % (model.w != 0).sum())
-
-    elif question == "2.5":
-        data = utils.load_dataset("logisticData")
-        XBin, yBin = data['X'], data['y']
-        XBinValid, yBinValid = data['Xvalid'], data['yvalid']
-
-        # TODO
-        logRegL2 = LogisticRegression('l2', C=1, solver='liblinear', fit_intercept=False)
-        logRegL2.fit(XBin, yBin)
-        logRegL2TrainErr = utils.classification_error(logRegL2.predict(XBin),yBin)
-        logRegL2ValidErr = utils.classification_error(logRegL2.predict(XBinValid),yBinValid)
+        utils.plotClassifier(lr, Xtrain, ytrain)
+        utils.savefig("logReg.png")
         
-        print("Scikit L2")
-        print("\nTraining error %.3f" % utils.classification_error(logRegL2.predict(XBin),yBin))
-        print("Validation error %.3f" % utils.classification_error(logRegL2.predict(XBinValid), yBinValid))
-        print("# nonZeros: %d" % (logRegL2.coef_ != 0).sum())
+        # kernel logistic regression with a linear kernel
+        lr_kernel = kernelLogRegL2(kernel_fun=logReg.kernel_linear, lammy=1)
+        lr_kernel.fit(Xtrain, ytrain)
 
-        logRegL1 = LogisticRegression('l1', C=1, solver='liblinear', fit_intercept=False)
-        logRegL1.fit(XBin, yBin)
-        logRegL1TrainErr = utils.classification_error(logRegL1.predict(XBin),yBin)
-        logRegL1ValidErr = utils.classification_error(logRegL1.predict(XBinValid),yBinValid)
+        print("Training error %.3f" % np.mean(lr_kernel.predict(Xtrain) != ytrain))
+        print("Validation error %.3f" % np.mean(lr_kernel.predict(Xtest) != ytest))
 
-        print("\n\nScikit L1")
-        print("\nTraining error %.3f" % utils.classification_error(logRegL1.predict(XBin),yBin))
-        print("Validation error %.3f" % utils.classification_error(logRegL1.predict(XBinValid), yBinValid))
-        print("# nonZeros: %d" % (logRegL1.coef_ != 0).sum())
+        utils.plotClassifier(lr_kernel, Xtrain, ytrain)
+        utils.savefig("logRegLinearKernel.png")
 
-    elif question == "3":
-        data = utils.load_dataset("multiData")
-        XMulti, yMulti = data['X'], data['y']
-        XMultiValid, yMultiValid = data['Xvalid'], data['yvalid']
+    elif question == "1.1":
+        dataset = load_dataset('nonLinearData.pkl')
+        X = dataset['X']
+        y = dataset['y']
 
-        model = linear_model.leastSquaresClassifier()
-        model.fit(XMulti, yMulti)
+        Xtrain, Xtest, ytrain, ytest = train_test_split(X,y,random_state=0)
 
-        print("leastSquaresClassifier Training error %.3f" % utils.classification_error(model.predict(XMulti), yMulti))
-        print("leastSquaresClassifier Validation error %.3f" % utils.classification_error(model.predict(XMultiValid), yMultiValid))
+        # YOUR CODE HERE
 
-        print(np.unique(model.predict(XMulti)))
+    elif question == "1.2":
+        dataset = load_dataset('nonLinearData.pkl')
+        X = dataset['X']
+        y = dataset['y']
 
+        Xtrain, Xtest, ytrain, ytest = train_test_split(X,y,random_state=0)
 
-    elif question == "3.2":
-        data = utils.load_dataset("multiData")
-        XMulti, yMulti = data['X'], data['y']
-        XMultiValid, yMultiValid = data['Xvalid'], data['yvalid']
+        # YOUR CODE HERE
 
-        model = linear_model.logLinearClassifier(maxEvals=500, verbose=0)
-        model.fit(XMulti, yMulti)
+    elif question == '4.1': 
+        X = load_dataset('highway.pkl')['X'].astype(float)/255
+        n,d = X.shape
+        print(n,d)
+        h,w = 64,64      # height and width of each image
 
-        print("logLinearClassifier Training error %.3f" % utils.classification_error(model.predict(XMulti), yMulti))
-        print("logLinearClassifier Validation error %.3f" % utils.classification_error(model.predict(XMultiValid), yMultiValid))
-        print("# nonZeros: %d" % (model.W != 0).sum())
+        k = 5            # number of PCs
+        threshold = 0.1  # threshold for being considered "foreground"
 
-    elif question == "3.4":
-        data = utils.load_dataset("multiData")
-        XMulti, yMulti = data['X'], data['y']
-        XMultiValid, yMultiValid = data['Xvalid'], data['yvalid']
+        model = AlternativePCA(k=k)
+        model.fit(X)
+        Z = model.compress(X)
+        Xhat_pca = model.expand(Z)
 
-        model = linear_model.softmaxClassifier(maxEvals=500)
-        model.fit(XMulti, yMulti)
+        model = RobustPCA(k=k)
+        model.fit(X)
+        Z = model.compress(X)
+        Xhat_robust = model.expand(Z)
 
-        print("Training error %.3f" % utils.classification_error(model.predict(XMulti), yMulti))
-        print("Validation error %.3f" % utils.classification_error(model.predict(XMultiValid), yMultiValid))
-        print("# nonZeros: %d" % (model.W != 0).sum())
+        fig, ax = plt.subplots(2,3)
+        for i in range(10):
+            ax[0,0].set_title('$X$')
+            ax[0,0].imshow(X[i].reshape(h,w).T, cmap='gray')
 
-    elif question == "3.5":
-        data = utils.load_dataset("multiData")
-        XMulti, yMulti = data['X'], data['y']
-        XMultiValid, yMultiValid = data['Xvalid'], data['yvalid']
+            ax[0,1].set_title('$\hat{X}$ (L2)')
+            ax[0,1].imshow(Xhat_pca[i].reshape(h,w).T, cmap='gray')
+            
+            ax[0,2].set_title('$|x_i-\hat{x_i}|$>threshold (L2)')
+            ax[0,2].imshow((np.abs(X[i] - Xhat_pca[i])<threshold).reshape(h,w).T, cmap='gray', vmin=0, vmax=1)
 
-        # TODO
+            ax[1,0].set_title('$X$')
+            ax[1,0].imshow(X[i].reshape(h,w).T, cmap='gray')
+            
+            ax[1,1].set_title('$\hat{X}$ (L1)')
+            ax[1,1].imshow(Xhat_robust[i].reshape(h,w).T, cmap='gray')
 
-        oneVSall = LogisticRegression(C=9999, multi_class='ovr', solver='liblinear', fit_intercept=False)
-        oneVSall.fit(XMulti, yMulti)
-        oneVSallTrainErr = utils.classification_error(oneVSall.predict(XMulti),yMulti)
-        oneVSallValidErr = utils.classification_error(oneVSall.predict(XMultiValid),yMultiValid)
+            ax[1,2].set_title('$|x_i-\hat{x_i}|$>threshold (L1)')
+            ax[1,2].imshow((np.abs(X[i] - Xhat_robust[i])<threshold).reshape(h,w).T, cmap='gray', vmin=0, vmax=1)
 
-        print("\nScikit Multiclass One vs All")
-        print("\nTraining error %.3f" % utils.classification_error(oneVSall.predict(XMulti),yMulti))
-        print("Validation error %.3f" % utils.classification_error(oneVSall.predict(XMultiValid), yMultiValid))
-        print("# nonZeros: %d" % (oneVSall.coef_ != 0).sum())
+            utils.savefig('highway_{:03d}.jpg'.format(i))
 
-
-        softmaxModel = LogisticRegression(C=9999, multi_class='multinomial', solver='lbfgs', fit_intercept=False, max_iter=200)
-        softmaxModel.fit(XMulti, yMulti)
-        softmaxModelTrainErr = utils.classification_error(softmaxModel.predict(XMulti),yMulti)
-        softmaxModelValidErr = utils.classification_error(softmaxModel.predict(XMultiValid),yMultiValid)
-
-        print("\nScikit Multiclass SoftMax")
-        print("\nTraining error %.3f" % utils.classification_error(softmaxModel.predict(XMulti),yMulti))
-        print("Validation error %.3f" % utils.classification_error(softmaxModel.predict(XMultiValid), yMultiValid))
-        print("# nonZeros: %d" % (softmaxModel.coef_ != 0).sum())
+    else:
+        print("Unknown question: %s" % question)    
